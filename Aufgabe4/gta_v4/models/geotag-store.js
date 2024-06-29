dedec41fa597617532a61c18642b4af25d2ad1a8
Ricewaffel <wassmeralexander@gmail.com>
2024-06-29 15:17:55 +0200

    Aufgabe 4 Vorbereitung

---
+++ Aufgabe4/gta_v4/models/geotag-store.js
@@ -0,0 +1,65 @@
+// File origin: VS1LAB A3
+
+const GeoTagExamples = require("./geotag-examples");
+const GeoTag = require("./geotag");
+
+/**
+ * This script is a template for exercise VS1lab/Aufgabe3
+ * Complete all TODOs in the code documentation.
+ */
+
+/**
+ * A class for in-memory-storage of geotags
+ * 
+ * Use an array to store a multiset of geotags.
+ * - The array must not be accessible from outside the store.
+ * 
+ * Provide a method 'addGeoTag' to add a geotag to the store.
+ * 
+ * Provide a method 'removeGeoTag' to delete geo-tags from the store by name.
+ * 
+ * Provide a method 'getNearbyGeoTags' that returns all geotags in the proximity of a location.
+ * - The location is given as a parameter.
+ * - The proximity is computed by means of a radius around the location.
+ * 
+ * Provide a method 'searchNearbyGeoTags' that returns all geotags in the proximity of a location that match a keyword.
+ * - The proximity constrained is the same as for 'getNearbyGeoTags'.
+ * - Keyword matching should include partial matches from name or hashtag fields. 
+ */
+class InMemoryGeoTagStore{
+
+    // TODO: ... your code here ...
+    #geoTags
+
+    constructor() {
+        this.#geoTags = [];
+        GeoTagExamples.tagList.forEach(tag => this.addGeoTag(new GeoTag(tag[0], tag[1], tag[2], tag[3])));
+    }
+
+    addGeoTag(geoTag){
+        this.#geoTags.push(geoTag);
+    }
+
+    removeGeoTag(name){
+        this.#geoTags = this.#geoTags.filter(geoTag => geoTag.name !== name);
+
+    }
+
+    getNearbyGeoTags(latitude, longitude, radius){
+        return this.#geoTags.filter(geoTag => {
+            const distance = Math.sqrt(Math.pow(geoTag.latitude - latitude, 2) + Math.pow(geoTag.longitude - longitude, 2));
+            return distance <= radius;
+        });
+    }
+
+    searchNearbyGeoTags(latitude, longitude, radius, keyword){
+        return this.getNearbyGeoTags(latitude, longitude, radius).filter(tag => {
+            return tag.name.toLowerCase().includes(keyword.toLowerCase()) || tag.hashtag.toLowerCase().includes(keyword.toLowerCase());
+        });
+    }
+
+
+
+}
+
+module.exports = InMemoryGeoTagStore
---
+++ Aufgabe4/gta_v4/public/javascripts/geotagging.js
@@ -0,0 +1,76 @@
+// File origin: VS1LAB A2
+
+/* eslint-disable no-unused-vars */
+
+// This script is executed when the browser loads index.html.
+
+// "console.log" writes to the browser's console. 
+// The console window must be opened explicitly in the browser.
+// Try to find this output in the browser...
+console.log("The geoTagging script is going to start...");
+
+
+
+/**
+ * TODO: 'updateLocation'
+ * A function to retrieve the current location and update the page.
+ * It is called once the page has been fully loaded.
+ */
+
+var updateLocation = function() {
+
+    const mapElement = document.getElementById("map");
+    const taglistJson = mapElement.getAttribute("data-tags");
+    const taglist = JSON.parse(taglistJson);
+
+    if (document.getElementById('latitude').value == "" || document.getElementById('longitude').value == "") {
+
+        LocationHelper.findLocation(   function (location) {
+            document.getElementById('latitude').value = location.latitude;
+            document.getElementById('longitude').value = location.longitude;
+
+            document.getElementById('search_latitude').value = location.latitude;
+            document.getElementById('search_longitude').value = location.longitude;
+
+            var map = new MapManager();
+            map.initMap(location.latitude, location.longitude);
+            map.updateMarkers(location.latitude, location.longitude, taglist);
+
+            var mapViewImage = document.getElementById('mapView');
+            if (mapViewImage) {
+                mapViewImage.remove();
+            }
+
+
+            var mapViewLabel = document.querySelector('.discovery__map span');
+            if (mapViewLabel) {
+            mapViewLabel.remove();
+            }
+        }
+   );
+                
+    } else {
+        var latitude = parseFloat(document.getElementById('latitude').value)
+        var longitude = parseFloat(document.getElementById('longitude').value)
+
+        var map = new MapManager();
+        map.initMap(latitude, longitude);
+        map.updateMarkers(latitude, longitude, taglist);
+
+        var mapViewImage = document.getElementById('mapView');
+            if (mapViewImage) {
+                mapViewImage.remove();
+            }
+
+
+            var mapViewLabel = document.querySelector('.discovery__map span');
+            if (mapViewLabel) {
+            mapViewLabel.remove();
+            }
+    }
+}
+
+// Wait for the page to fully load its DOM content, then call updateLocation
+document.addEventListener("DOMContentLoaded", () => {
+    updateLocation();
+});
